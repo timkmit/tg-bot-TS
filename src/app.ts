bffb0038c57469d069198c1c396925a55b1a3660
Timur <timengalytchev@mail.ru>
2024-03-17 19:50:08 +0300

    made: command class

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import { Telegraf, session } from "telegraf";
 import { IConfigService } from "./config/config.interface";
 import { ConfigService } from "./config/config.service";
 import { IBotContext } from "../context/context.interface";
+import { Command } from "./commands/command.class";
 
 class Bot {
 
     bot: Telegraf<IBotContext>;
+    commands: Command[] = [];
 
     constructor(private readonly configService: IConfigService){
         this.bot = new Telegraf<IBotContext>(this.configService.get("TOKEN"));
@@ -13,7 +15,12 @@ class Bot {
     }
 
     init(){
+
+        for (const command of this.commands){
+            command.handle();
+        }
         this.bot.launch();
+        
     }
 
 }
